aebf236645c30f90f24a0ca5208f24e6749d0c84
fabrice zander <fabricezander@gmail.com>
2016-09-07 15:44:00 +0200

    fabsie

--- oefeningen/fileread.c
+++ oefeningen/fileread.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
-int main (void){
+int main (void)
+{
 
-#define charcount 1024 ;
-char path[charcount];
-FILE * input;
 
-input=fgets(path *s);
-fputs(path *s);
+char path[1024];
+char output[2048];
+
+fgets(path, 1024, stdin);
+fputs(path,  stdout);
+
+
+FILE *contents;
+path[strlen(path) - 1] = '\0';
+contents=fopen(path,"r");
+
+fread(output,1,1024,contents);
+int i=0;
+for(i=0; i<1024;i++){
+	printf("%c",output[i]);
+}
 
 }
